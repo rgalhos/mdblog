2a4e57ab5ab5792b3721cc5ede019bb872c10f86
Rafael Galhós <rgmg@ic.ufal.br>
2022-08-09 10:38:59 -0300

    Remove unused functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,3 @@
-import { readFileSync } from 'fs';
-import type { IHistory } from './interfaces/IHistory';
-import type { IHistoryEntry } from './interfaces/IHistoryEntry';
-
 export function bytesToSizeString(bytes: number, precision = 1): string {
     const sizes = [null, 'B', 'KB', 'MB', 'GB', 'TB'];
     let i = 0;
@@ -10,28 +6,6 @@ export function bytesToSizeString(bytes: number, precision = 1): string {
     while ((i++, bytes > 1000)) bytes /= 1 << 10;
 
     return bytes.toFixed(i === 1 ? 0 : precision) + ' ' + sizes[i];
-}
-
-export function getEditHistory(path: string): IHistoryEntry[] {
-    const history: IHistory = JSON.parse(readFileSync('./history.json').toString('utf-8'));
-
-    if (!Object.prototype.hasOwnProperty.call(history.hashDict, path)) {
-        return [];
-    }
-
-    const entries: IHistoryEntry[] = [];
-
-    let hash: IHistoryEntry['previousHash'] = history.hashDict[path];
-    let entry: IHistoryEntry;
-    while (typeof hash === 'string' && (entry = history.entries[hash]) && hash !== entry.previousHash) {
-        entry = history.entries[hash];
-        entries.push(entry);
-        hash = entry.previousHash;
-    }
-
-    entries.shift();
-
-    return entries;
 }
 
 export function wait(ms: number): Promise<void> {
